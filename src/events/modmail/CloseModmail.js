74d1dae7c888e45c0630453daa499b2af47b1bc8
Mr.Fantasma <contact@mrfantasma.com>
2025-05-27 19:22:20 -0400

    feat(modmail): ✨close modmail threads
    Adds functionality to close modmail threads.
    
    - Implements a button interaction to close threads
    - Sends a closing message to the user
    - Deletes the modmail thread after a delay
    - Logs errors if closing or deletion fails

---
+++ src/events/modmail/CloseModmail.js
@@ -0,0 +1,70 @@
+import { EmbedBuilder, Events, MessageFlags } from 'discord.js';
+import { t } from 'i18next';
+
+import { Logger } from '#utils/Logger.js';
+import Modmail from '#models/Modmail.js';
+
+const logger = new Logger('CloseModmail');
+
+export default class CloseModmail {
+  name = Events.InteractionCreate;
+  once = false;
+
+  /**
+   * @param {import('discord.js').Interaction} interaction
+   * @returns {Promise<void>}
+   */
+  async execute(interaction) {
+    if (!interaction.isButton() || interaction.customId !== 'close_modmail') return;
+
+    try {
+      const thread = await Modmail.findOne({ where: { channelId: interaction.channel.id } });
+      if (!thread) return await interaction.reply({ content: t('messages:modmail.close.notFound'), flags: MessageFlags.Ephemeral });
+
+      const user = await interaction.client.users.fetch(thread.userId);
+
+      if (user) {
+        const closeEmbed = new EmbedBuilder()
+          .setTitle(t('messages:modmail.close.title'))
+          .setDescription(t('messages:modmail.close.description', { user: interaction.user.displayName, guild: interaction.guild.name }))
+          .addFields([
+            {
+              name: t('messages:modmail.close.fields.staff'),
+              value: `**${interaction.user.displayName}** (${interaction.user.id})`,
+              inline: true,
+            },
+            {
+              name: t('messages:modmail.close.fields.guild'),
+              value: `**${interaction.guild.name}** (${interaction.guild.id})`,
+              inline: true,
+            },
+            {
+              name: t('messages:modmail.close.fields.date'),
+              value: `<t:${Math.floor(Date.now() / 1000)}:F>`,
+              inline: false,
+            },
+          ])
+          .setColor('Blurple')
+          .setFooter({
+            text: t('messages:modmail.close.footer', { guild: interaction.guild.name }) || t('messages:modmail.close.uknownGuild'),
+            iconURL: interaction.guild.iconURL() || undefined,
+          })
+          .setTimestamp();
+
+        await user.send({ embeds: [closeEmbed] });
+        await Modmail.destroy({ where: { channelId: interaction.channel.id } });
+        await interaction.reply({ content: t('messages:modmail.close.threadClose'), flags: MessageFlags.Ephemeral });
+
+        setTimeout(async () => {
+          try {
+            await interaction.channel.delete();
+          } catch (err) {
+            logger.fatal('Failed to delete modmail thread', err);
+          }
+        }, 5000);
+      }
+    } catch (err) {
+      logger.error('Failed to close modmail thread', err);
+    }
+  }
+}
