a161e108ec2a6766be2772dc27dbadb5ad77b9b8
Mr.Fantasma <contact@mrfantasma.com>
2025-05-28 21:32:44 -0400

    fix(ready): 🐛 improves client typing and imports
    - updates client typing to include ExtendedClient
    - ensures correct import paths for utils and structures

--- src/events/client/ready.ts
+++ src/events/client/ready.ts
@@ -1,6 +1,7 @@
-import { Events } from 'discord.js';
+import { Client, Events } from 'discord.js';
 
-import { Logger } from '#utils/Logger.js';
+import { Logger } from '#utils/Logger';
+import { ExtendedClient } from '#structures/ExtendedClient';
 
 const logger = new Logger('Ready');
 
@@ -8,11 +9,7 @@ export default class Ready {
   name = Events.ClientReady;
   once = true;
 
-  /**
-   * @param {import('discord.js').Client} client
-   * @returns {Promise<void>}
-   */
-  async execute(client) {
+  async execute(client: Client<true> & ExtendedClient) {
     logger.info(`Logged in as "${client.user.displayName}" (${client.user.id})`);
     logger.info(`Guilds: ${client.guilds.cache.size}`);
     logger.info(`Slash Commands: ${client.slashCommands.size}`);
